de384b17cec1e3d748043bf6a30686fbbc73542b
rikku47 <rikku47@users.noreply.github.com>
2018-06-19 11:45:31 +0200

    Added website and email to the about window and increased the width of textbox groupname.

--- SWC/About.xaml.cs
+++ SWC/About.xaml.cs
@@ -1,16 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Diagnostics;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace SWC
 {
@@ -22,6 +11,18 @@ namespace SWC
         public About()
         {
             InitializeComponent();
+        }
+
+        private void HlKaiFriebePageSWC_RequestNavigate(object sender, System.Windows.Navigation.RequestNavigateEventArgs e)
+        {
+            Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri));
+            e.Handled = true;
+        }
+
+        private void hlEmail_RequestNavigate(object sender, System.Windows.Navigation.RequestNavigateEventArgs e)
+        {
+            Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri));
+            e.Handled = true;
         }
     }
 }
